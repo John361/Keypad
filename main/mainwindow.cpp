1b0fe4244133fb01a554d1ac44737f9915ff7d8b
Hoareau John <hxjwork1@gmail.com>
2018-06-05 23:54:16 +0400

    Need save file

--- main/mainwindow.cpp
+++ main/mainwindow.cpp
@@ -4,13 +4,17 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     m_action = new Action(this);
     m_editor = new QTextEdit;
-    m_textStream = 0;
+    m_file = 0;
 
     addAction(m_action->newWindow());
     addAction(m_action->openFile());
+    addAction(m_action->saveFile());
+    addAction(m_action->leaveApp());
 
     connect(m_action->newWindow(), SIGNAL(triggered(bool)), this, SLOT(openNewWindow()));
     connect(m_action->openFile(), SIGNAL(triggered(bool)), this, SLOT(openFile()));
+    connect(m_action->saveFile(), SIGNAL(triggered(bool)), this, SLOT(saveFile()));
+    connect(m_action->leaveApp(), SIGNAL(triggered(bool)), this, SLOT(close()));
 
     setCentralWidget(m_editor);
 }
@@ -20,19 +24,47 @@ void MainWindow::openFile()
     const QString fileName = QFileDialog::getOpenFileName(this);
     QFile file(fileName);
 
-    if (!file.open(QIODevice::ReadOnly))
+    if (!file.open(QIODevice::ReadWrite))
     {
         QMessageBox::warning(this, tr("Warning"), tr("The file cannot be opened"));
         return;
     }
 
-    m_textStream = new QTextStream(&file);
-    m_editor->setText(m_textStream->readAll());
-    file.close();
+    QTextStream *textStream = new QTextStream(&file);
+    m_editor->setText(textStream->readAll());
+    delete textStream;
 }
 
 void MainWindow::openNewWindow()
 {
     MainWindow *mw = new MainWindow;
     mw->show();
+}
+
+void MainWindow::saveFile()
+{
+    if (m_file == 0)
+    {
+        // TODO : create a new file
+        return;
+    }
+
+    else
+    {
+        if (!m_file->open(QIODevice::ReadWrite))
+        {
+            QMessageBox::warning(this, tr("Warning"), tr("The file cannot be opened"));
+            return;
+        }
+
+        QString text = m_editor->toPlainText();
+        qint64 n = m_file->write(text.toUtf8());
+        m_file->close();
+
+        if (n == 0)
+        {
+            QMessageBox::warning(this, tr("Warning"), tr("The file cannot be saved"));
+            return;
+        }
+    }
 }
--- main/mainwindow.h
+++ main/mainwindow.h
@@ -19,18 +19,25 @@ class MainWindow : public QMainWindow
 
     private:
         Action *m_action;
+        QFile *m_file;
         QTextEdit *m_editor;
-        QTextStream *m_textStream;
 
     signals:
-        //void textStreamChanged();
 
     public slots:
+        /**
+         * @brief openFile opens a file and writes text in the editor
+         */
         void openFile();
+
+        /**
+         * @brief saveFile save text from the editor in the file
+         */
+        void saveFile();
 
     private slots:
         /**
-         * @brief openNewWindow
+         * @brief openNewWindow opens a new MainWindow
          */
         void openNewWindow();
 };
